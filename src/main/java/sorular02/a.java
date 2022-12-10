7b38514794c6cc0cc645bbe0c7354842d6717539
Erkam KILINÇ <erkamkilinc55@gmail.com>
2022-12-10 05:03:43 +0300

    continue

--- src/main/java/sorular02/a.java
+++ src/main/java/sorular02/a.java
@@ -2,14 +2,4 @@ package sorular02;
 
 public class a {
 
-    public static void main(String[] args) {
-
-        assert false;
-        assert false;
-        assert false;
-        assert false;
-        assert false;
-        assert false;
-        assert false;
-    }
 }
