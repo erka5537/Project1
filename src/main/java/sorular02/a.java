6ccaf79ffb687823d23221416494bee8e79a1add
Erkam KILINÇ <erkamkilinc55@gmail.com>
2022-12-18 02:38:46 +0300

    continue

--- src/main/java/sorular02/a.java
+++ src/main/java/sorular02/a.java
@@ -4,6 +4,6 @@ public class a {
 
     public static void main(String[] args) {
 
-        System.out.println("as888777894568asasaaddadsasdaadasaasaadaaaa");
+        System.out.println("aas888777894568asasaaddadsasdaadasaasaadaaaa");
     }
 }
