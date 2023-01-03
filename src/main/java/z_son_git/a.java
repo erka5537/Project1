d011950c2c76d452b2af8d8ceb8d125629a47632
Erkam KILINÇ <erkamkilinc55@gmail.com>
2023-01-03 05:18:55 +0300

    continue

---
+++ src/main/java/z_son_git/a.java
@@ -0,0 +1,12 @@
+package z_son_git;
+
+public class a {
+
+    public static void main(String[] args) {
+
+        System.out.println("hbmbaqgexvezzxtaaaasmmmmm88b8aaa" +
+                "777894568asasaaddaadsamsdaandasaaasaaadaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaa");
+    }
+}
