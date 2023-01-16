4ff97fe44e4584862a247c6f637915725da61e4c
Erkam KILINÇ <erkamkilinc55@gmail.com>
2023-01-16 23:45:56 +0300

    image sizes have been changed

--- src/main/java/z_son_git/RockPaperScissors.java
+++ src/main/java/z_son_git/RockPaperScissors.java
@@ -36,7 +36,6 @@ public class RockPaperScissors {
         win.setResizable(false);
 
         //rock buton
-
         this.rock = new JButton(new ImageIcon("C:\\Users\\ERKA55\\Desktop\\TaşKağıtMakas\\tas.png"));
         rock.setBounds(150, 250, 50, 50);
         rock.setBorderPainted(false);
