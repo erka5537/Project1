c80a5ec4d4b0f348e074bb39ed19a5c112259dd4
Erkam KILINÇ <erkamkilinc55@gmail.com>
2023-01-30 03:52:48 +0300

    image sizes have been changed

--- src/main/java/z_son_git/RockPaperScissors.java
+++ src/main/java/z_son_git/RockPaperScissors.java
@@ -173,6 +173,6 @@ public class RockPaperScissors {
     }
 
     public static void main(String[]args) {
-        new RockPaperScissors();//llllllllllllllllllllllllllll
+        new RockPaperScissors();//lllllllllllllllllllllllllllll
     }
 }
