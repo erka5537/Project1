cffc0c894ab2e6cac27224cce3a913391edcec85
Erkam KILINÇ <erkamkilinc55@gmail.com>
2023-03-01 19:00:41 +0300

    image sizes have been changed

--- src/main/java/z_son_git/RockPaperScissors.java
+++ src/main/java/z_son_git/RockPaperScissors.java
@@ -175,13 +175,8 @@ public class RockPaperScissors {
         }
 
     }
-    private static Icon resizeIcon(ImageIcon icon, int resizedWidth, int resizedHeight) {
-        Image img = icon.getImage();
-        Image resizedImage = img.getScaledInstance(resizedWidth, resizedHeight,  java.awt.Image.SCALE_SMOOTH);
-        return new ImageIcon(resizedImage);
-    }
 
     public static void main(String[]args) {
-        new RockPaperScissors(); //lllllllllllllllllllllllll
+        new RockPaperScissors();
     }
 }
